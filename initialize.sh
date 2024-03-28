1bbc65d7aa664371f4d1b9e21e211e71febea2b5
Arashi <13753619+ArashiJF@users.noreply.github.com>
2024-03-28 09:35:48 -0500

    - Ensure system dependencies exist for pyenv.

--- initialize.sh
+++ initialize.sh
@@ -4,6 +4,11 @@ if [ -e ~/.pyenv ]; then
     echo ".pyenv was already there, getting rid of it" && rm -rf ~/.pyenv
 fi
 
+# Add the system dependencies for pyenv
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 curl https://pyenv.run | zsh
 
 # Add vscodium extensions
